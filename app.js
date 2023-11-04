260c9874357acf0bec4eb5e17e4b0678be247482
kopki <kopki684@gmail.com>
2023-11-05 02:21:43 +0800

    commit

--- app.js
+++ app.js
@@ -31,6 +31,16 @@ app.use(helmet())
 app.use(cors())
 app.use(xss())
 
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
+
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs api</h1><a href="/api-docs">Documentation</a>')
+})
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 
 app.use('/api/v1/auth', authRouter);
